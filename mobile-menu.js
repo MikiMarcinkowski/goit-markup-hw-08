88980393efc9691692c5b625ef719bfa413d28f7
mikus151 <mikus151@wp.pl>
2022-11-30 15:14:09 +0100

    summary

---
+++ mobile-menu.js
@@ -0,0 +1,27 @@
+// const menuOverlay = document.querySelector('.mobile-menu');
+// const closeMenuButton = document.querySelector('.toggle-menu');
+// const openMenuBtn = document.querySelector('.header__link-logo');
+
+// openMenuBtn.addEventListener('click', (e) => {
+//   menuOverlay.classList.remove('d-none');
+// });
+
+// closeMenuButton.addEventListener('click', (e) => {
+//   menuOverlay.classList.add('d-none');
+// });
+
+(() => {
+  const menuOpenBtn = document.querySelector('[data-menu-open]');
+  const menuCloseBtn = document.querySelector('[data-menu-close]');
+
+  const mobileMenu = document.querySelector('[data-menu]');
+  // const body = document.querySelector('body');
+
+  menuOpenBtn.addEventListener('click', openModal);
+  menuCloseBtn.addEventListener('click', openModal);
+
+  function openModal() {
+    mobileMenu.classList.toggle('is-open');
+    // body.classList.toggle('no-scroll');
+  }
+})();
